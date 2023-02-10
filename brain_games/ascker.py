cab8c29fcc7acc244dd7daac504a0bc72ec8da81
Timofey <timayakovishin@gmail.com>
2023-02-10 11:58:30 +0300

    deleted file from main dir

--- brain_games/ascker.py
+++
@@ -1,9 +0,0 @@
-#!usr/bin/env python3
-import prompt
-
-
-def ask_u_str(question):
-    """Askign a user question and return the unswer"""
-    print(f'Question: {question}')
-    answer = prompt.string('Your answer: ')
-    return answer
