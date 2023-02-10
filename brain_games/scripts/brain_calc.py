6ca107716a35da246547aee06106755ebbd6a522
Timofey <timayakovishin@gmail.com>
2023-02-10 16:36:35 +0300

    new function

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -5,29 +5,39 @@ from brain_games.greeting.greeting import welcome_user
 from brain_games.answer_loop.answer_loop import answer_loop
 from brain_games.asker.asker import ask_you
 
-def calc(operand_a:int, operand_b: int, operand: str) -> int:
+
+def calc(operand_a: int, operand_b: int, operand: str) -> int:
     operations = {"+": operand_a + operand_b,
-                 "-": operand_a - operand_b,
-                 "*": operand_a * operand_b }
+                  "-": operand_a - operand_b,
+                  "*": operand_a * operand_b}
     return operations.get(operand)
 
 
-@answer_loop
-def main(name: str) -> str|tuple:
-    print("What is the result of the expression?")
-    operators = ['+','-','*']
+def create_example() -> tuple:
+    operators = ['+', '-', '*']
     operand_a = randint(1, 100)
     operand_b = randint(1, 100)
     symbol = choice(operators)
+    return operand_a, operand_b, symbol
+
+
+@answer_loop
+def brain_calc(name: str) -> str | tuple:
+    operand_a, operand_b, symbol = create_example()
     example = f'{operand_a} {symbol} {operand_b}'
     answer = calc(operand_a, operand_b, symbol)
     response = ask_you(example)
-    if response == answer:
+    if response == str(answer):
         return 'Correct!'
     else:
-        return response, answer    
+        return response, answer
+
+
+def main():
+    print("What is the result of the expression?")
+    name = welcome_user()
+    brain_calc(name)
 
 
 if __name__ == '__main__':
-    name = welcome_user()
-    main(name)
+    main()
