ea899745b00dc2ea346a882c41d51e9cc6b6a834
Timofey <timayakovishin@gmail.com>
2023-02-10 14:01:14 +0300

    modifided the loop function

--- brain_games/answer_loop/answer_loop.py
+++ brain_games/answer_loop/answer_loop.py
@@ -2,21 +2,28 @@
 from string import digits
 
 
+def check_value(res):
+    if isinstance(res, str):
+        print('Correct!')
+    else:
+        que = "\'" if res[0] in digits else "\""
+        user_answer = f"{que}{res[0]}{que} is wrong answer ;(."
+        correct_answer = f"Correct answer was {que}{res[1]}{que}."
+        print(user_answer, correct_answer)
+
+
 def answer_loop(fn):
     def wrapper(name=''):
         counter = 0
         while counter < 3:
             res = fn(name)
             if res == 'Correct!':
-                print('Correct!')
+                check_value(res)
                 counter += 1
                 if counter == 3:
                     print(f"Congratulations, {name}")
             else:
-                que = "\'" if res[0] in digits else "\""
-                user_answer = f"{que}{res[0]}{que} is wrong answer ;(."
-                correct_answer = f"Correct answer was {que}{res[1]}{que}."
-                print(user_answer, correct_answer,
-                      f"\nLet's try again, {name}!")
+                check_value(res)
+                print(f"Let's try again, {name}!")
                 break
     return wrapper
